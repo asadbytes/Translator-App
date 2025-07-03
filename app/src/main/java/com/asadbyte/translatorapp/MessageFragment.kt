c7a040fa2272d5d53144a3ecb84bcbb750ec4c23
asadbytes <asadbyte@gmail.com>
2025-07-03 11:50:58 +0500

    before voice translation

--- app/src/main/java/com/asadbyte/translatorapp/MessageFragment.kt
+++ app/src/main/java/com/asadbyte/translatorapp/MessageFragment.kt
@@ -6,14 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.navGraphViewModels
 import com.asadbyte.translatorapp.databinding.FragmentMessageBinding
-
-// Other imports remain the same
+import com.asadbyte.translatorapp.main.HomeFragmentDirections
+import com.asadbyte.translatorapp.main.HomeViewModel
+import com.asadbyte.translatorapp.translation.LanguageKeys.KEY_SOURCE
+import com.asadbyte.translatorapp.translation.LanguageKeys.KEY_TARGET
+import com.asadbyte.translatorapp.translation.LanguageKeys.REQUEST_KEY
 
 class MessageFragment : Fragment() {
     private var _binding: FragmentMessageBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: HomeViewModel by navGraphViewModels(R.id.nav_graph)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,9 +32,33 @@ class MessageFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel.sourceLanguage.observe(viewLifecycleOwner) { languageName ->
+            binding.languageButtonUser1.text = languageName
+        }
+        viewModel.targetLanguage.observe(viewLifecycleOwner) { languageName ->
+            binding.languageButtonUser2.text = languageName
+        }
+
+        setFragmentResultListener(REQUEST_KEY) { _, bundle ->
+            // We have a result!
+            val selectedLanguage = bundle.getString("selectedLanguage")
+            val requesterKey = bundle.getString("requesterKey")
+
+            // Update the correct TextView based on who made the request
+            if (selectedLanguage != null) {
+                when (requesterKey) {
+                    KEY_SOURCE -> viewModel.updateSourceLanguage(selectedLanguage)
+                    KEY_TARGET -> viewModel.updateTargetLanguage(selectedLanguage)
+                }
+            }
+        }
+
         val homeIcon = binding.bottomNavigation.findViewById<View>(R.id.bottom_nav_home)
         val cameraIcon = binding.bottomNavigation.findViewById<View>(R.id.bottom_nav_camera)
 
+        binding.swapIcon.setOnClickListener {
+            viewModel.swapLanguages()
+        }
 
         cameraIcon.setOnClickListener {
             findNavController().navigate(R.id.action_messageFragment_to_cameraHomeFragment)
@@ -41,14 +71,14 @@ class MessageFragment : Fragment() {
         // Button for User 1 (no rotation)
         binding.languageButtonUser1.setOnClickListener {
             // We pass 'false' for the argument, or don't pass it to use the default value
-            val action = MessageFragmentDirections.actionMessageFragmentToLanguageSelectionFragment(isForUser2 = false, requesterKey = "source")
+            val action = MessageFragmentDirections.actionMessageFragmentToLanguageSelectionFragment(isForUser2 = false, requesterKey = KEY_SOURCE)
             findNavController().navigate(action)
         }
 
         // Button for User 2 (will be rotated)
         binding.languageButtonUser2.setOnClickListener {
             // We pass 'true' for the argument to indicate this is for User 2
-            val action = MessageFragmentDirections.actionMessageFragmentToLanguageSelectionFragment(isForUser2 = true, requesterKey = "target")
+            val action = MessageFragmentDirections.actionMessageFragmentToLanguageSelectionFragment(isForUser2 = true, requesterKey = KEY_TARGET)
             findNavController().navigate(action)
         }
     }
